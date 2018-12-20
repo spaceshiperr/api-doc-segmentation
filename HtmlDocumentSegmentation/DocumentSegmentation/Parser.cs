8b092fb40b6bb5df58f65f5f84ab82c93ab791fe
Dinara Faizullina <imfd@protonmail.com>
2018-12-21 00:42:23 +0300

    Replace doc body with its main content

--- HtmlDocumentSegmentation/DocumentSegmentation/Parser.cs
+++ HtmlDocumentSegmentation/DocumentSegmentation/Parser.cs
@@ -5,19 +5,14 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using HtmlAgilityPack;
+using System.IO;
 
 namespace DocumentSegmentation
 {
     public class Parser
     {
-        public Parser(Uri url)
-        {
-            Article = ParseArticle(url);
-        }
-
-        private Article Article { get; set; }
-
-        private Article ParseArticle(Uri url)
+        public Article GetArticle(Uri url)
         {
             using (WebClient web = new WebClient())
             {
@@ -29,6 +24,47 @@ namespace DocumentSegmentation
                 var article = JsonConvert.DeserializeObject<Article>(str);
                 return article;
             }
+        }
+
+        public HtmlDocument GetHtmlDocument(Uri url)
+        {
+            var web = new HtmlWeb();
+            var document = web.Load(url);
+            return document;
+        }
+
+        public HtmlDocument GetUpdatedHtmlDocument(Article article)
+        {
+            var url = new Uri(article.url);
+            var document = GetHtmlDocument(url);
+
+            ReplaceDocumentBody(document, article.content);
+            return document;
+        }
+
+        public void ReplaceDocumentBody(HtmlDocument document, string replacement)
+        {
+            var body = document.DocumentNode.SelectNodes("//body").First();
+
+            HtmlNode newBody = document.CreateElement("body");
+            newBody.InnerHtml = replacement;
+
+            body.ParentNode.ReplaceChild(newBody, body);
+        }
+
+        public void AppendHepsNode(HtmlDocument document)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\scripts\heps.js");
+            var text = string.Empty;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            HtmlNode hepsNode = HtmlNode.CreateNode("<script>" + text + "</script>");
+            var body = document.DocumentNode.SelectSingleNode("//body");
+            body.AppendChild(hepsNode);
         }
     }
 }
--- HtmlDocumentSegmentation/DocumentSegmentation/Program.cs
+++ HtmlDocumentSegmentation/DocumentSegmentation/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Newtonsoft.Json;
 
 namespace DocumentSegmentation
@@ -13,7 +15,13 @@ namespace DocumentSegmentation
         {
             var url = new Uri("https://www.kernel.org/doc/html/latest/kernel-hacking/hacking.html#introduction");
 
-            var parser = new Parser(url);
+            var parser = new Parser();
+            var article = parser.GetArticle(url);
+            var document = parser.GetUpdatedHtmlDocument(article);
+            parser.AppendHepsNode(document);
+
+            FileStream sw = new FileStream("index.html", FileMode.Create);
+            document.Save(sw);
         }
     }
 }
